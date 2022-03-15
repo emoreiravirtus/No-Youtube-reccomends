ab68d89891c80bb91b48166a0b11d2ebbd3fd768
Elias Moreira <56691312+emoreiravirtus@users.noreply.github.com>
2022-03-15 11:42:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ setInterval(() => {
   if(document.querySelector('#contents')) {
     document.querySelector('#contents').innerHTML = null;
   }
+  
+  if(document.querySelector('#main') && window.location.href == 'https://www.linkedin.com/feed/') {
+    document.querySelector('#main').innerHTML = null;
+  }
 
   if(document.querySelector('#chips-wrapper')) {
     document.querySelector('#chips-wrapper').innerHTML = null;
